78f69078a8ce10076c52def2e6bea7b09b2250ec
Ahmed Hussien <ahmedhgabr25@gmail.com>
2026-01-06 18:35:53 +0200

    feat: add form ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import BUEForm from './BUEForm'
 
 function App() {
 
   return (
     <>
       <h1>BEU Registration Form</h1>
+      <BUEForm />
     </>
   )
 }
---
+++ src/BUEForm.tsx
@@ -0,0 +1,63 @@
+import './App.css'
+
+function BUEForm() {
+
+    return (
+        <>
+        
+            <form>
+                <div className="space-y-12">
+                    <div className="border-b border-white/10 pb-12">
+                        <h2 className="text-base/7 font-semibold text-white">Personal Information</h2>
+                        <p className="mt-1 text-sm/6 text-gray-400">Use a permanent address where you can receive mail.</p>
+
+                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div className="sm:col-span-3">
+                                <label htmlFor="first-name" className="block text-sm/6 font-medium text-white">First name</label>
+                                <div className="mt-2">
+                                    <input id="first-name" type="text" name="first-name" autoComplete="given-name" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                                </div>
+                            </div>
+
+                            <div className="sm:col-span-4">
+                                <label htmlFor="email" className="block text-sm/6 font-medium text-white">Email address</label>
+                                <div className="mt-2">
+                                    <input id="email" type="email" name="email" autoComplete="email" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                                </div>
+                            </div>
+
+                            <div className="sm:col-span-3">
+                                <label htmlFor="country" className="block text-sm/6 font-medium text-white">Country</label>
+                                <div className="mt-2 grid grid-cols-1">
+                                    <select id="country" name="country" autoComplete="country-name" className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white/5 py-1.5 pr-8 pl-3 text-base text-white outline-1 -outline-offset-1 outline-white/10 *:bg-gray-800 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6">
+                                        <option>United States</option>
+                                        <option>Canada</option>
+                                        <option>Mexico</option>
+                                    </select>
+                                    <svg viewBox="0 0 16 16" fill="currentColor" data-slot="icon" aria-hidden="true" className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-400 sm:size-4">
+                                        <path d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" fillRule="evenodd" />
+                                    </svg>
+                                </div>
+                            </div>
+
+                            <div className="sm:col-span-2">
+                                <label htmlFor="postal-code" className="block text-sm/6 font-medium text-white">ZIP / Postal code</label>
+                                <div className="mt-2">
+                                    <input id="postal-code" type="text" name="postal-code" autoComplete="postal-code" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                    <button type="button" className="text-sm/6 font-semibold text-white">Cancel</button>
+                    <button type="submit" className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+                </div>
+            </form>
+
+        </>
+    )
+}
+
+export default BUEForm
